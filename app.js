cfe6bf0fb53268c332cd7c2fba875eebf4275a7c
Andrew Noble <andrew.noble@affinitas.de>
2017-01-10 16:33:28 +0100

    t

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 var pm2       = require('pm2');
 var SysLogger = require('ain2');
-var logger    = new SysLogger({tag: 'pm2',  facility: 'local1', hostname: ''});
-
+var logger    = new SysLogger({tag: 'pm2',  facility: 'local1'});
+logger.set({hostname: ''});
 logger.setMessageComposer(function(message, severity){
   return new Buffer('<' + (this.facility * 8 + severity) + '> ' + message);
 });
